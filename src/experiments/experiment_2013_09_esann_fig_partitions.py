e88f1ac02c59164940c4f3dc837b429db2d3b3ea
Björn <bjoernwe@users.noreply.github.com>
2014-01-06 12:52:58 +0100

    esann experiments added

---
+++ src/experiments/experiment_2013_09_esann_fig_partitions.py
@@ -0,0 +1,104 @@
+"""
+Generates plots for the ESANN paper.
+"""
+
+from matplotlib import pyplot
+
+import worldmodel
+from experiment_2013_02_noisy_dim import NoisyDimData
+from experiment_2013_03_random_walk_swissroll import RandomSwissRollData
+
+            
+if __name__ == '__main__':
+    
+    #
+    # parameters
+    #
+    resolution = 1000
+    data_all = 5000
+    data_training = 5000
+
+    #
+    # data
+    # 
+    data_generators = [RandomSwissRollData,
+                       NoisyDimData]
+    data_list = []
+    for i, generator in enumerate(data_generators):
+        data = generator(n=data_all, seed=0)
+        data_list.append(data)
+    # scale swiss role
+    data_list[0].data += 1.
+    data_list[0].data /= 2.
+
+    models = {}
+    models['swiss_naive'] = worldmodel.WorldModel(method='naive')
+    models['swiss_predictive'] = worldmodel.WorldModel(method='spectral')
+    models['noise_naive'] = worldmodel.WorldModel(method='naive')
+    models['noise_predictive'] = worldmodel.WorldModel(method='spectral')
+
+
+    #
+    # swiss role, predictive
+    #
+    model = models['swiss_predictive']
+    model.add_data(data=data_list[0].data[:data_training], actions=data.actions)
+    model.update_stats()
+
+    for i in range(16-1):
+        model.single_splitting_step(min_gain=float('-inf'))
+        
+    pyplot.subplot(1, 3, 1)
+    model.plot_data(color='none', show_plot=False)
+    #model.plot_states(show_plot=False, resolution=resolution)
+    model.plot_state_borders(show_plot=False, resolution=resolution)
+    pyplot.title('a')
+    pyplot.xlabel('x')
+    pyplot.ylabel('y')
+
+                
+    #
+    # noise, predictive
+    #
+    model = models['noise_predictive']
+    model.add_data(data=data_list[1].data[:data_training], actions=data.actions)
+    model.update_stats()
+  
+    for _ in range(8-1):
+        model.single_splitting_step(min_gain=float('-inf'))
+        # plot data and borders
+ 
+    pyplot.subplot(1, 3, 2)
+    #model.plot_data(color='none', show_plot=False)
+    model.plot_state_borders(show_plot=False, resolution=resolution)
+    pyplot.title('b')
+    pyplot.xlabel('x')
+    #pyplot.ylabel('y')
+                  
+    #
+    # noise, naive
+    #
+    model = models['noise_naive']
+    model.add_data(data=data_list[1].data, actions=data.actions)
+    model.update_stats()
+  
+    for _ in range(3):
+        for leaf in model.tree.get_leaves():
+            leaf._apply_split(allow_useless_split=True)
+        for leaf in model.tree.get_leaves():
+            leaf._apply_split(allow_useless_split=True)
+        model.update_stats()
+  
+    # plot
+    pyplot.subplot(1, 3, 3)
+    #model.plot_data(color='silver', show_plot=False)
+    model.plot_state_borders(show_plot=False, resolution=resolution)
+    pyplot.title('c')
+    pyplot.xlabel('x')
+    #pyplot.ylabel('y')
+     
+ 
+    #pyplot.tight_layout()
+    #pyplot.subplots_adjust(wspace=0.2, hspace=0.3)
+    pyplot.subplots_adjust(bottom=0.12, top=0.9, wspace=0.1)
+    pyplot.show()
---
+++ src/experiments/experiment_2013_09_esann_fig_result.py
@@ -0,0 +1,105 @@
+"""
+Generates plots for the ESANN paper.
+"""
+
+import cPickle as pickle
+import numpy as np
+
+from matplotlib import pyplot
+
+            
+if __name__ == '__main__':
+    
+    #
+    # parameters
+    #
+    T = 10
+    resolution = 100
+
+
+    #
+    # load data
+    #
+    results = {}
+    for t in range(T):
+        results[t] = {}
+        results[t]['models'] = pickle.load(open('experiment_2013_09_esann_models_%d.dump' % t, 'rb') )
+        results[t]['data'] = pickle.load(open('experiment_2013_09_esann_data_%d.dump' % t, 'rb') )
+        print len(results[t]['models']['swiss_predictive'].stats)
+        print len(results[t]['models']['noise_predictive'].stats)
+        print ''
+        
+    #
+    # plot results swiss role 
+    #
+    N = 24
+    pyplot.subplot(1, 2, 1)
+    pyplot.xlabel('number of states')
+    pyplot.ylabel('mutual information')
+    pyplot.title('Problem A')
+
+    results_mi = np.zeros((T, 4))
+    results_nodes = np.zeros((T, 4))
+    for t in range(T):
+        model = results[t]['models']['swiss_naive']
+        results_mi[t,:] = np.array([s.mutual_information for s in model.stats])[:4]
+        results_nodes[t,:] = np.array([s.n_states for s in model.stats])[:4]
+    n_states = np.mean(results_nodes, axis=0)
+    mi_mean = np.mean(results_mi, axis=0)
+    mi_std = np.std(results_mi, axis=0)
+    pyplot.errorbar(x=n_states, y=mi_mean, yerr=mi_std, fmt='o-')
+    
+    results_mi = np.zeros((T, N))
+    results_nodes = np.zeros((T, N))
+    for t in range(T):
+        model = results[t]['models']['swiss_predictive']
+        results_mi[t,:] = np.array([s.mutual_information for s in model.stats])[:N]
+        results_nodes[t,:] = np.array([s.n_states for s in model.stats])[:N]
+    n_states = np.mean(results_nodes, axis=0)
+    mi_mean = np.mean(results_mi, axis=0)
+    mi_std = np.std(results_mi, axis=0)
+    pyplot.errorbar(x=n_states, y=mi_mean, yerr=mi_std, fmt='-')
+
+    pyplot.legend(['naive', 'predicitve'], loc='lower right')
+
+
+    #
+    # plot results noise 
+    #
+    N = 16
+    pyplot.subplot(1, 2, 2)
+    pyplot.xlabel('number of states')
+    pyplot.title('Problem B')
+    
+    results_mi = np.zeros((T, 4))
+    results_nodes = np.zeros((T, 4))
+    for t in range(T):
+        model = results[t]['models']['noise_naive']
+        results_mi[t,:] = np.array([s.mutual_information for s in model.stats])[:4]
+        results_nodes[t,:] = np.array([s.n_states for s in model.stats])[:4]
+    n_states = np.mean(results_nodes, axis=0)
+    mi_mean = np.mean(results_mi, axis=0)
+    mi_std = np.std(results_mi, axis=0)
+    pyplot.errorbar(x=n_states, y=mi_mean, yerr=mi_std, fmt='o-')
+
+    results_mi = np.zeros((T, N))
+    results_nodes = np.zeros((T, N))
+    for t in range(T):
+        model = results[t]['models']['noise_predictive']
+        results_mi[t,:] = np.array([s.mutual_information for s in model.stats])[:N]
+        results_nodes[t,:] = np.array([s.n_states for s in model.stats])[:N]
+    n_states = np.mean(results_nodes, axis=0)
+    mi_mean = np.mean(results_mi, axis=0)
+    mi_std = np.std(results_mi, axis=0)
+    pyplot.errorbar(x=n_states, y=mi_mean, yerr=mi_std, fmt='-')
+
+    pyplot.legend(['naive', 'predicitve'], loc='lower right')
+
+    #
+    # 
+    #
+    #pyplot.subplots_adjust(left=0.05, right=0.95, wspace=0.1)
+    pyplot.subplots_adjust(bottom=0.12, top=0.9, wspace=0.1)
+    #pyplot.subplots_adjust(wspace=0.1)
+    pyplot.show()
+    
